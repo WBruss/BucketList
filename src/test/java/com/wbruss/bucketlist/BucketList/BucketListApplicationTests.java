ffc67b465c6f37c186fb6c845a100f728a0a626d
Paul Wainaina <paul.wainaina@tracom.co.ke>
2021-02-23 17:18:39 +0300

    Initial Commit

---
+++ src/test/java/com/wbruss/bucketlist/BucketList/BucketListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbruss.bucketlist.BucketList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
